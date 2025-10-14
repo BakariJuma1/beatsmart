83759fed4158ad94d88030415f1ffb8021f27124
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-10-14 10:31:27 +0300

    Fix:Updated my user model and schema to fit firebase

--- server/schemas/user_schema.py
+++ server/schemas/user_schema.py
@@ -1,5 +1,5 @@
 from server.extension import ma
-from marshmallow import fields, validate, post_load
+from marshmallow import fields
 from server.models.user import User
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
@@ -7,17 +7,5 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
         model = User
         load_instance = True
         include_fk = True
-        exclude = ("password_hash",)
 
-    password = fields.String(load_only=True, required=True, validate=validate.Length(min=6))
     created_at = fields.DateTime(dump_only=True)
-
-    @post_load
-    def make_user(self, data, **kwargs):
-        """Automatically hash password on load"""
-        if "password" in data:
-            pw = data.pop("password")
-            user = User(**data)
-            user.set_password(pw)
-            return user
-        return User(**data)
