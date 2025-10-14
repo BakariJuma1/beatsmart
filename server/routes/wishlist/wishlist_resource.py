aa603d8b39f8c89d8ba2b0be0fb51db61aa7469b
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-10-14 12:37:50 +0300

    Feat:Adding my routes

--- server/routes/wishlist/wishlist_resource.py
+++ server/routes/wishlist/wishlist_resource.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource,Api
 from flask import request
 from server.models.wishlist import Wishlist
 from server.models.beat import Beat
@@ -6,7 +6,9 @@ from server.models.soundpack import SoundPack
 from server.schemas.wishlist_schema import WishlistSchema
 from server.extension import db
 from server.utils.firebase_auth import firebase_auth_required
+from . import wishlist_resource_bp
 
+api = Api(wishlist_resource_bp)
 wishlist_schema = WishlistSchema()
 wishlists_schema = WishlistSchema(many=True)
 
@@ -57,3 +59,5 @@ class WishlistResource(Resource):
         db.session.delete(wishlist_item)
         db.session.commit()
         return {"message": "Item removed from wishlist"}, 200
+    
+api.add_resource(WishlistResource, "/wishlist", "/wishlist/<int:wishlist_id>")
