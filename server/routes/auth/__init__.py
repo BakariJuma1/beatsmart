1d168a6d0d14f8b22d17baf8884efa19eb4a7567
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-10-14 10:54:44 +0300

    Fix:Acleanup on my  auth routes and using blueprints

--- server/routes/auth/__init__.py
+++ server/routes/auth/__init__.py
@@ -3,7 +3,4 @@ from flask import Blueprint
 auth_bp = Blueprint('auth_bp',__name__)
 
 
-from .login import *
-from .signup import *
-from .forgot_password import *
-from .verify import *
+from .verify_token import *
--- server/routes/auth/me.py
+++ server/routes/auth/me.py
@@ -8,12 +8,11 @@ api = Api(auth_bp)
 
 user_schema = UserSchema()
 
-class VerifyTokenResource(Resource):
+
+class MeResource(Resource):
     @firebase_auth_required
     def get(self):
         user = request.current_user
         return user_schema.dump(user), 200
 
-
-
-api.add_resource(VerifyTokenResource, "/auth/verify-token")
+api.add_resource(MeResource, "/auth/me")
