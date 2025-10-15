87c4f2a0c4eb1f470a5921a793899aa09bab7ecc
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-10-15 10:37:05 +0300

    fix:switching to ffmeg for audio cutting its supported by latest python

--- backend/server/utils/audio_utils.py
+++ backend/server/utils/audio_utils.py
@@ -1,25 +1,19 @@
-from pydub import AudioSegment
+import ffmpeg
 import tempfile
-import os
 
-PREVIEW_DURATION_MS = 30 * 1000 
-
+PREVIEW_DURATION_MS = 30 * 1000
 def create_preview(file_storage, start_time=0):
-  
     try:
-        
-        audio = AudioSegment.from_file(file_storage)
-
-        start_ms = start_time * 1000
-        end_ms = start_ms + PREVIEW_DURATION_MS
-        if end_ms > len(audio):
-            end_ms = len(audio)
-
-        preview = audio[start_ms:end_ms]
-
-        
         temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
-        preview.export(temp_file.name, format="mp3")
+
+        (
+            ffmpeg
+            .input(file_storage, ss=start_time, t=PREVIEW_DURATION_MS / 1000)
+            .output(temp_file.name, format='mp3', acodec='libmp3lame')
+            .overwrite_output()
+            .run(quiet=True)
+        )
+
         return temp_file.name
     except Exception as e:
         print("Preview generation failed:", e)
